12163091e09fe58a65f7f4d0493cf69c7e402565
Tomasz <slimak98@gmail.com>
2020-10-28 00:55:28 +0100

    removal of a file is now possible

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function StyledDropzone(props) {
         const files = props.files;
         for (let file of acceptedFiles) {
             if (!files.find(f => f.name === file.name && f.size === file.size)) {
-                console.log('pushing ' + file.name + ', size: ' + file.size)
                 files.push(file);
             }
         }
@@ -42,6 +41,36 @@ function StyledDropzone(props) {
 function App() {
     const [files, setFiles] = useState([]);
 
+    const removeFile = file => {
+        const id = files.indexOf(file);
+        let filesCopy = [...files];
+        filesCopy.splice(id, 1)
+        setFiles(filesCopy);
+    }
+
+    const trashIcon = () => {
+        return (
+            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-trash" fill="currentColor"
+                 xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd"
+                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg>
+        )
+    }
+
+    const parseSize = size => {
+        const units = ['K', 'M', 'G', 'T'];
+        let unitIndex = 0;
+        size /= 1024;
+        while (size > 1000) {
+            size = size / 1000;
+            unitIndex++;
+        }
+        return `${Math.round(size*100)/100} ${units[unitIndex]}B`;
+    }
+
     return (
         <div className="App">
             <header className="App-header">
@@ -55,7 +84,10 @@ function App() {
                     <StyledDropzone files={files} setFiles={setFiles}/>
                     <ul className="file-list">
                         {files.map(file =>
-                            <li key={file.name + file.size}>{file.name}</li>
+                            <li key={file.name + file.size}>
+                                <div className="left">{file.name}</div>
+                                <div className="right" onClick={() => removeFile(file)}>{trashIcon()}</div>
+                            </li>
                         )}
                     </ul>
                 </section>
