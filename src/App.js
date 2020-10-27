6814516347f64e9e48deb68232237819e9b024c9
Tomasz <slimak98@gmail.com>
2020-10-27 21:19:43 +0100

    Landing page and dropzone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import {useDropzone} from "react-dropzone";
+import {useCallback, useState} from "react";
+
+
+function StyledDropzone(props) {
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        const setFiles = props.setFiles;
+        const files = props.files;
+        for (let file of acceptedFiles) {
+            if (!files.find(f => f.name === file.name && f.size === file.size)) {
+                console.log('pushing ' + file.name + ', size: ' + file.size)
+                files.push(file);
+            }
+        }
+        setFiles([...files]);
+    }, [props.files, props.setFiles])
+
+    const {
+        getRootProps,
+        getInputProps,
+        isDragActive,
+        isDragAccept,
+        isDragReject
+    } = useDropzone({onDrop});
+
+    return (
+        <div className="container">
+            <div className="styledDrop" drag={isDragActive ? 'true' : 'false'} {...getRootProps()}>
+                <input {...getInputProps()} />
+                {!isDragActive ? <p>Przeciągnij tutaj swoje pliki <br/> lub <br/> kliknij, by je wybrać</p> :
+                    <p>Upuść teraz</p>}
+            </div>
+            {/*{files.map(file =>*/}
+            {/*    <li>{file.name}</li>*/}
+            {/*)}*/}
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [files, setFiles] = useState([]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Swiftshare</h1>
+                <p>
+                    Udostępniaj szybko i wygodnie
+                </p>
+            </header>
+            <main>
+                <section>
+                    <StyledDropzone files={files} setFiles={setFiles}/>
+                    <ul className="filelist">
+                        {files.map(file =>
+                            <li key={file.name + file.size}>{file.name}</li>
+                        )}
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
