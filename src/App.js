c8a6363d12228a0c8b309de8214ff55cc3ca384b
Tomasz <tomaszjanik17@gmail.com>
2020-11-14 20:58:15 +0100

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -96,15 +96,15 @@ function App() {
         )
     }
 
-    const checkIcon = () => {
-        return (
-            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="currentColor"
-                 xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd"
-                      d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
-            </svg>
-        )
-    }
+    // const checkIcon = () => {
+    //     return (
+    //         <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-check" fill="currentColor"
+    //              xmlns="http://www.w3.org/2000/svg">
+    //             <path fillRule="evenodd"
+    //                   d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+    //         </svg>
+    //     )
+    // }
 
     const parseSize = size => {
         const units = ['K', 'M', 'G', 'T'];
@@ -121,7 +121,7 @@ function App() {
     const [urls, setUrls] = useState({});
     const [uploads, setUploads] = useState(0);
     const [showToast, setShowToast] = useState(false);
-    const [toastTimeout, setToastTimeout] = useState(undefined);
+    const [, setToastTimeout] = useState(undefined);
 
     const uploadFiles = toUpload => {
         if (!toUpload || toUpload.length === 0 || uploads > 0) {
