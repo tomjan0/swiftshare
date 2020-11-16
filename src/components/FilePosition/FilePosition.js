3a38c0ea9add7de2166a400ffd939bb275e1b4f3
Tomasz <slimak98@gmail.com>
2020-11-16 02:10:15 +0100

    begin splitting app into components

---
+++ src/components/FilePosition/FilePosition.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./FilePosition.css";
+
+export default class FilePosition extends React.Component {
+    render() {
+        return (
+            <div className="FilePosition"><p className="test">FilePosition Works!</p></div>
+        )
+    }
+}
