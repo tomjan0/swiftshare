7a59ae6ac1a86d3ff402e754479b970ff8a2db22
Tomasz <slimak98@gmail.com>
2021-02-18 17:56:04 +0100

    Rebuilt responses from cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,58 +1,52 @@
 const functions = require('firebase-functions');
 const speech = require('@google-cloud/speech');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-const options = {
-    timeoutSeconds: 540,
-    memory: '1GB'
-}
+// Creates a client
+const client = new speech.SpeechClient();
 
-exports.speechToText = functions.region('europe-west3').runWith(options).https.onCall(async (data) => {
+exports.speechToText = functions.region('europe-west3').https.onCall(async (data) => {
+    try {
+        if (!data.path || !data.type) {
+            return {status: 'ERROR', message: 'Wrong request data.'}
+        }
+        const gcsUri = `gs://swiftshare-df351.appspot.com/${data.path}`;
+        const config = {
+            languageCode: 'pl-PL',
+        };
 
-    if (!data.path) {
-        return JSON.stringify({avgConfidence: '', transcription: 'File not found'})
+        if (data.type === 'wav') {
+            config.encoding = 'LINEAR16';
+        } else if (data.type === 'mp3') {
+            config.sampleRateHertz = 44100;
+        }
+
+        const audio = {
+            uri: gcsUri,
+        };
+
+        const req = {
+            config: config,
+            audio: audio,
+        };
+
+        // Detects speech in the audio file. This creates a recognition job that you
+        // can wait for now, or get its result later.
+        const [operation] = await client.longRunningRecognize(req);
+        // Get a Promise representation of the final result of the job
+        const res = (await operation.promise())[0];
+        if (res.results) {
+            const transcription = res.results
+                .map(result => result.alternatives[0].transcript)
+                .join('\n');
+            const sumConfidence = res.results
+                .map(result => result.alternatives[0].confidence)
+                .reduce((acc, el) => acc + el)
+            const avgConfidence = sumConfidence / res.results.length;
+            return {status: "OK", transcription, avgConfidence};
+        }
+        return {status: "ERROR", message: 'Empty transcription.'};
+    } catch (e) {
+        functions.logger.error(e);
+        return {status: "ERROR", message: e.details || 'Unknown error occurred.'};
     }
-    // Creates a client
-    const client = new speech.SpeechClient();
-
-    const gcsUri = `gs://swiftshare-df351.appspot.com/${data.path}`;
-    const encoding = 'LINEAR16';
-    // const sampleRateHertz = 16000;
-    const languageCode = 'pl-PL';
-
-    const config = {
-        // encoding: encoding,
-        // sampleRateHertz: sampleRateHertz,
-        languageCode: languageCode,
-    };
-
-    if (data.type === 'wav') {
-        config.encoding = encoding;
-    }
-
-    const audio = {
-        uri: gcsUri,
-    };
-
-    const req = {
-        config: config,
-        audio: audio,
-    };
-
-    // Detects speech in the audio file. This creates a recognition job that you
-    // can wait for now, or get its result later.
-    const [operation] = await client.longRunningRecognize(req);
-    // Get a Promise representation of the final result of the job
-    const [res] = await operation.promise();
-    const transcription = res.results
-        .map(result => result.alternatives[0].transcript)
-        .join('\n');
-    const sumConfidence = res.results
-        .map(result => result.alternatives[0].confidence)
-        .reduce((acc, el) => acc + el)
-    const avgConfidence = sumConfidence / res.results.length;
-    // functions.logger.info(`Transcription: ${transcription}`);
-    return JSON.stringify({transcription, avgConfidence});
 });
